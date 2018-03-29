06c783968296c70dd425a144ba762de7ef28418e
lokmane <lokmane.krizou@student.oulu.fi>
2018-03-29 11:54:47 +0300

    added UsageStateManager to get recent Apps opened added checkForPermission to be opened automatically for usage states

--- focus/app/src/main/java/com/derassom/three_pixels/focus/MainActivity.java
+++ focus/app/src/main/java/com/derassom/three_pixels/focus/MainActivity.java
@@ -1,7 +1,34 @@
 package com.derassom.three_pixels.focus;
 
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.AppOpsManager;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +36,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //run intent service (not used for now)
+        Intent i= new Intent(this,runningAppService.class);
+        startService(i);
+
+    }
+
+    protected void onResume() {
+
+        super.onResume();
+        if (!checkForPermission(this)){
+       // Create alertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.permission_dialog_message)
+                .setTitle(R.string.permission_dialog_title);
+        builder.setPositiveButton(R.string.request, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked request button
+                startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        AlertDialog dialog = builder.create();
+            dialog.show();
+            }
+
+    }
+    //check for permission of enabled usage data
+    private boolean checkForPermission(Context context) {
+        boolean granted = false;
+        AppOpsManager appOps = (AppOpsManager) context
+                .getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), context.getPackageName());
+
+        if (mode == AppOpsManager.MODE_DEFAULT) {
+            granted = (context.checkCallingOrSelfPermission(android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED);
+        } else {
+            granted = (mode == AppOpsManager.MODE_ALLOWED);
+        }
+    return granted;
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+   // test button
+    public void runningInfo(View view) {
+        //declare UsageStateManager to get the recent used apps
+        UsageStatsManager usageStates = (UsageStatsManager) this.getSystemService( Context.USAGE_STATS_SERVICE);
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.DATE, -1);
+        long startTime = calendar.getTimeInMillis();
+
+        List<UsageStats> usageStatsList = usageStates.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,startTime,endTime);
+        ArrayList<String> packageNames= new ArrayList<String>();
+        for (UsageStats u : usageStatsList){
+            if(u.getPackageName()!=null){
+                packageNames.add(u.getPackageName());
+            Log.d(TAG, "PakageName: " + u.getPackageName() +  "\t" + "ForegroundTime: "
+                    + u.getTotalTimeInForeground() + " milliseconds") ;}
+
+        }
+
+
     }
 }
